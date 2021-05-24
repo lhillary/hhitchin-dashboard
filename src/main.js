2755e4e9d32fe5041060e91ad331de8bcb849d35
Lydia Hillary <44858988+lhillary@users.noreply.github.com>
2021-05-24 11:04:25 -0400

    adding entire v1 dashboard

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import VueGoogleCharts from 'vue-google-charts'
+import { domain, clientId, audience } from "../auth_config.json";
+import { Auth0Plugin } from "./auth";
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
+
+Vue.use(VueGoogleCharts)
+Vue.use(require("moment"))
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Messages from '../views/Messages.vue'
+import Table from '../views/Table.vue'
+import Login from '../views/Login.vue'
+import { authGuard } from "../auth/authGuard";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    beforeEnter: authGuard
+  },
+  {
+    path: '/messages',
+    name: 'Messages',
+    component: Messages,
+    beforeEnter: authGuard
+  },
+  {
+    path: '/table',
+    name: 'Table',
+    component: Table,
+    beforeEnter: authGuard
   }
 ]
 
